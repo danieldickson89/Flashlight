74783f49516db71150d726ae644b1538fd6c7a39
Daniel Dickson <danieldickson89@gmail.com>
2016-02-01 18:51:17 -0700

    Done

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var onButton: UIButton!
+    
+    var isOn = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func buttonTapped(sender: UIButton) {
+        if isOn {
+            isOn = false
+            self.view.backgroundColor = UIColor.blackColor()
+            onButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            onButton.setTitle("Off", forState: .Normal)
+            
+        }
+        else
+        {
+            isOn = true
+            self.view.backgroundColor = UIColor.whiteColor()
+            onButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            onButton.setTitle("On", forState: .Normal)
+        }
+    }
 }
 
